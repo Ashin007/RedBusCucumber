950f49cfd86811aeac18de7b2abee5b82b09bb51
Ashin Vijayan <ashinvijayan007@gmail.com>
2025-07-23 18:50:29 +0530

    headerless

--- src/test/java/com/redbus/MyTest.java
+++ src/test/java/com/redbus/MyTest.java
@@ -2,21 +2,31 @@ package com.redbus;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 import java.time.Duration;
 
 public class MyTest {
+    WebDriver driver;
     @Test
     public void sampleTest(){
 
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get("https://www.redbus.in/");
         System.out.println(driver.getTitle());
         System.out.println(driver.getPageSource());
         driver.close();
-        driver.quit();
+    }
+    @AfterTest
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
     }
 }
